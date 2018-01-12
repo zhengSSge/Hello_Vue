70bb725131b86ff3cb0a5efa87d8a3c042a0a45c
zhengzhen <827807932@qq.com>
2018-01-12 14:08:49 +0800

    test

--- sell/src/components/shopchar/shopchar.vue
+++ sell/src/components/shopchar/shopchar.vue
@@ -1,5 +1,7 @@
 <template>
   <!--该组件是通过外部传入数据计算总价以及数量的-->
+  <!--该组件是通过外部传入数据计算总价以及数量的-->
+  <!--该组件是通过外部传入数据计算总价以及数量的-->
   <div class="shopchar">
     <div class="content" @click="toggleShow">
       <div class="content-left">
